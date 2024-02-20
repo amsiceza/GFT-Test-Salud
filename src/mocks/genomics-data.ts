6978eb65935a788db1235e659ba339e2dd9b573b
amsiceza <ismacerverazamora@gmail.com>
2024-02-20 14:29:24 +0100

    Add functionality state of items corousel

--- src/mocks/genomics-data.ts
+++ src/mocks/genomics-data.ts
@@ -1,8 +1,26 @@
- export const genomicsData = [
+export const genomicsData = [
   {
     titulo: 'Consulta con el cardiólogo',
     subtitulo: 'Revisión de la presión arterial y el colesterol',
-    fecha: '22/02/2024',
+    fecha: '20/02/2024',
+    estado: 'realizado',
+  },
+  {
+    titulo: 'Consulta con el dermatólogo',
+    subtitulo: 'Tratamiento para el acné y las manchas',
+    fecha: '05/03/2024',
+    estado: 'realizado',
+  },
+  {
+    titulo: 'Consulta con el psicólogo',
+    subtitulo: 'Sesión de terapia cognitivo-conductual',
+    fecha: '12/03/2024',
+    estado: 'realizado',
+  },
+  {
+    titulo: 'Consulta con el alergólogo',
+    subtitulo: 'Pruebas de alergia y tratamiento antihistamínico',
+    fecha: '19/03/2024',
     estado: 'realizado',
   },
   {
@@ -12,22 +30,10 @@
     estado: 'pendiente',
   },
   {
-    titulo: 'Análisis de sangre',
-    subtitulo: 'Detectar posibles alteraciones en el organismo',
-    fecha: '28/02/2024',
-    estado: 'deshabilitado',
-  },
-  {
     titulo: 'Consulta con el dentista',
     subtitulo: 'Limpieza dental y revisión de las encías',
     fecha: '01/03/2024',
     estado: 'pendiente',
-  },
-  {
-    titulo: 'Consulta con el dermatólogo',
-    subtitulo: 'Tratamiento para el acné y las manchas',
-    fecha: '05/03/2024',
-    estado: 'realizado',
   },
   {
     titulo: 'Consulta con el oftalmólogo',
@@ -36,27 +42,21 @@
     estado: 'pendiente',
   },
   {
-    titulo: 'Consulta con el psicólogo',
-    subtitulo: 'Sesión de terapia cognitivo-conductual',
-    fecha: '12/03/2024',
-    estado: 'realizado',
-  },
-  {
     titulo: 'Consulta con el nutricionista',
     subtitulo: 'Evaluación del peso y el índice de masa corporal',
     fecha: '15/03/2024',
     estado: 'pendiente',
   },
   {
-    titulo: 'Consulta con el alergólogo',
-    subtitulo: 'Pruebas de alergia y tratamiento antihistamínico',
-    fecha: '19/03/2024',
-    estado: 'realizado',
-  },
-  {
     titulo: 'Consulta con el endocrinólogo',
     subtitulo: 'Control de la diabetes y la tiroides',
     fecha: '22/03/2024',
     estado: 'pendiente',
+  },
+  {
+    titulo: 'Análisis de sangre',
+    subtitulo: 'Detectar posibles alteraciones en el organismo',
+    fecha: '22/02/2025',
+    estado: 'deshabilitado',
   },
 ];
